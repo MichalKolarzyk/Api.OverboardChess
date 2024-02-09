32f200638c06b82a74ef0c7c41334a88b6e31150
LAPTOP-L1T0L126\micha <michalkolarzyk@gmail.com>
2024-02-09 21:47:09 +0100

    add exception loggin

--- Http.OverboardChess/Controllers/ExceptionControllers/ExceptionController.cs
+++ Http.OverboardChess/Controllers/ExceptionControllers/ExceptionController.cs
@@ -1,19 +1,31 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace Http.OverboardChess.Controllers.ExceptionControllers
 {
     [Route("api/[controller]")]
     [ApiExplorerSettings(IgnoreApi = true)]
     [ApiController]
-    public class ExceptionController : ControllerBase
+    public class ExceptionController(ILogger<ExceptionController> logger) : ControllerBase
     {
+
+
         [Route("/exception")]
         public IActionResult HandleException()
         {
             var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>()!;
             var exception = exceptionHandlerFeature.Error;
+
+            var logMessage = new StringBuilder()
+                .AppendLine("title: ")
+                .Append(exception.Message)
+                .AppendLine("detail: ")
+                .Append(exception.StackTrace)
+                .ToString();
+
+            logger.LogError(logMessage);
 
             return Problem(detail: exception.StackTrace, title: exception.Message);
         }
